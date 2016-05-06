5793ada69b09efe490807afbc8945b16ad686792
seiza <jacques@couvreur.me>
2016-05-06 09:18:11 +0200

    Update game.updated field when save a Game and can save all games.

--- app/renderer.js
+++ app/renderer.js
@@ -5,7 +5,8 @@
 //var remote = require('electron').remote;
 // $scope.games = remote.getGlobal('games');
 
-var fs = require('fs');
+// var fs = require('fs');
+var fs = require('fs.extra');
 
 const shell = require('electron').shell;
 
@@ -38,11 +39,33 @@ angularApp.controller('MagaMain', function ($scope) {
 
     $scope.editionButtonClicked = function() {
         if ($scope.selectedGame) {
+            if ($scope.editing) {
+                $scope.selectedGame.updated = new Date();
+            }
             $scope.editing = !$scope.editing;
         } else {
             $scope.editing = false;
         }
         updateEditionButton();
+    };
+
+    $scope.saveButtonClicked = function() {
+        // backup
+        var newPath = path.replace(".json", "-"+new Date().toISOString()+".json").replace(/:/g, '-');
+        // console.log('>>>>> COPY: from:', path);
+        // console.log('>>>>> COPY: to  :', newPath);
+        // fs.createReadStream(path).pipe(fs.createWriteStream(newPath));
+        fs.copy(path, newPath, { replace: false }, function (err) {
+            if (err) { throw err; }
+            console.log("JSON backuped as:", newPath);
+
+            // save
+            var json = JSON.stringify($scope.games, null, 4);
+            fs.writeFile(path, json, function (err) {
+                if (err) return console.log(err);
+                console.log('JSON saved!');
+            });
+        });
     };
 
     var updateEditionButton = function () {
